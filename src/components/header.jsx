8eed11e4b3e705fb8975cfdcecc88ec4b9338b11
taiyangAh <wulian404@gmail.com>
2022-05-02 23:16:42 +0800

    header组件连接metaMask

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
 import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 export default function Header() {
+  const [accounts, setAccounts] = useState("connect");
+
+  async function handleConnect() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const chainId = await window.ethereum.request({ method: "eth_chainId" });
+      if (chainId !== "0x4") {
+        alert("Please connect to Rinkeby");
+      } else {
+        let account = accounts[0];
+        let formatAccount = account.substr(0, 8) + "..." + account.substr(-4);
+        setAccounts(formatAccount);
+      }
+    } else {
+      alert("please install metaMask");
+    }
+  }
+
   return (
     <div className="header">
       <div className="logoContainer">
@@ -31,7 +51,9 @@ export default function Header() {
         </div>
       </div>
       <div className="login">
-        <button className="loginButton">connect</button>
+        <button className="loginButton" onClick={handleConnect}>
+          {accounts}
+        </button>
       </div>
     </div>
   );
